d26e1d01822f465d4c5827b3897d611cd1bbe314
Matic Zagmajster <maticzagmajster@gmail.com>
2023-12-18 15:03:49 +0100

    Update callback subscriber after testing

--- EventListener/CallbackSubscriber.php
+++ EventListener/CallbackSubscriber.php
@@ -12,8 +12,37 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\Transport\Dsn;
 
+/**
+ * Supported webhook events by Omnivery
+ *
+ * evevnt              O M
+ * delivered           * *
+ * failed              * x
+ * rejected            * x
+ * opened              * *
+ * clicked             * *
+ * unsubscribed        * *
+ * complained          * *
+ * accepted            x *
+ * permanent_fail      x *
+ * temporary_fail      x *
+ *
+ * More info here: https://docs.omnivery.com/docs/mailgun-api-v3/5cc9374300b99-webhooks#webhook-event-types
+ * 
+ */
+
 class CallbackSubscriber implements EventSubscriberInterface
 {
+    const WEBHOOK_MESSAGE_EVENTS = [
+        'delivered',
+        'failed',
+        'rejected',
+        'opened',
+        'clicked',
+        'unsubscribed',
+        'complained',
+    ];
+
     public function __construct(
         private TransportCallback $transportCallback,
         private CoreParametersHelper $coreParametersHelper,
@@ -33,6 +62,92 @@ class CallbackSubscriber implements EventSubscriberInterface
         return '';
     }
 
+    private function isValidMessageEvent(array $eventData): bool {
+        return in_array($eventData['event'], self::WEBHOOK_MESSAGE_EVENTS);
+    }
+
+    private function processCallbackByEmailAddress(?string $recipient, array $eventData): void {
+        $event = $eventData['event'];
+        $deliveryStatus = $eventData['delivery-status'] ?? [];
+        $messageHeaders = $eventData['message']['headers'] ?? [];
+        $severity = $eventData['severity'] ?? null;
+        $channelId = $this->getEmailChannelId($messageHeaders);
+
+        if (isset($deliveryStatus['description'])) {
+            $comments = $deliveryStatus['description'];
+        }
+        else {
+            $comments = $deliveryStatus['message'];
+        }
+
+        $type            = null;  // reason in database
+        $canUseChannelId = true;
+        switch ($event) {
+            case 'bounce':
+            case 'permanent_fail':
+
+                $type = DoNotContact::BOUNCED;
+                break;
+
+            case 'failed':
+                switch ($severity) {
+                    case 'permanent':
+                        $type = DoNotContact::BOUNCED;
+                        break;
+
+                    case 'temporary':
+                    case 'softbounce':
+                        $type            = DoNotContact::BOUNCED;
+                        $canUseChannelId = false;
+                        break;
+
+                    default:
+                        break;
+                }
+                break;
+
+            case 'rejected':
+                $type            = DoNotContact::IS_CONTACTABLE;
+                $canUseChannelId = false;
+                break;
+
+            case 'complained':
+                $type = DoNotContact::UNSUBSCRIBED;
+                break;
+
+            case 'unsubscribed':
+                $comments = 'unsubscribed';
+                $type     = DoNotContact::UNSUBSCRIBED;
+                break;
+
+            default:
+                break;
+        }
+
+        if ($type === null) {
+            // It does not appear that there is anyhing wrong 
+            // with the message. Nothing else to do here :).
+            return;
+        }
+
+        if (null !== $channelId && $canUseChannelId) {
+            $this->transportCallback->addFailureByAddress(
+                $recipient,
+                $comments,
+                $type,
+                $channelId
+            );
+        } else {
+            $this->transportCallback->addFailureByAddress(
+                $event['recipient'],
+                $comments,
+                $type,
+                null
+            );
+        }
+
+    }
+
     /**
      * @return array<string, string>
      */
@@ -46,119 +161,66 @@ class CallbackSubscriber implements EventSubscriberInterface
     public function processCallbackRequest(TransportWebhookEvent $event): void
     {
         $dsn = Dsn::fromString($this->coreParametersHelper->get('mailer_dsn'));
-
         if ('mautic+omnivery+api' !== $dsn->getScheme()) {
             return;
         }
 
-        $this->logger->debug('Start processCallbackRequest');
         $postData = json_decode($event->getRequest()->getContent(), true);
-
-        if (is_array($postData) && isset($postData['event_data'])) {
+        $eventData = null;
+        $this->logger->debug('Start processCallbackRequest, incomming request', ['postData' => $postData]);
+        
+        if (is_array($postData) && isset($postData['event-data'])) {
             // Omnivery API callback
-            $events = [
-                $postData['event_data'],
-            ];
+            $eventData = $postData['event-data'];
         } else {
-            // Response must be an array.
-            $event->setResponse(new Response('Omnivery Integration: Could not get event data.'));
+            $event->setResponse(
+                new Response(
+                    json_encode([
+                        'message' => 'Could not find event-data key, not processing.',
+                        'success' => false
+                    ]),
+                    Response::HTTP_BAD_REQUEST,
+                    ['content-type' => 'application/json']
+                )
+            );
 
+            $this->logger->error(
+                'OmniveryTransportCallbackSubscriber: Could not process webhook request for payload.',
+                ['payload' => $postData]
+            );
             return;
         }
 
-        foreach ($events as $event) {
-            $this->logger->debug('Event '.$event['event']);
-            if (!in_array($event['event'], ['bounce', 'rejected', 'complained', 'unsubscribed', 'permanent_fail', 'failed'])) {
-                continue;
-            }
+        if (!$this->isValidMessageEvent($eventData)) {
+            $event->setResponse(
+                new Response(
+                    json_encode([
+                        'message' => sprintf('Unrecognized event type: "%s".', $eventData['event']),
+                        'success' => false
+                    ]),
+                    Response::HTTP_UNPROCESSABLE_ENTITY,
+                    ['content-type' => 'application/json']
+                )
+            );
 
-            // Try to get a description of an error.
-            $reason = $event['event'];
-            if (isset($event['delivery-status'], $event['delivery-status']['message'], $event['delivery-status']['message'][0])) {
-                $reason = $event['delivery-status']['message'][0];
-            } elseif (isset($event['delivery-status'], $event['delivery-status']['description'])) {
-                $reason = $event['delivery-status']['description'];
-            }
-
-            // Get error severity.
-            $severity = null;
-            if (isset($event['severity'])) {
-                $severity = $event['severity'];
-            }
-
-            $type            = null;
-            $canUseChannelId = true;
-            switch ($event['event']) {
-                case 'bounce':
-                case 'permanent_fail':
-                    $type = DoNotContact::BOUNCED;
-                    break;
-
-                case 'failed':
-                    switch ($severity) {
-                        case 'permanent':
-                            $type = DoNotContact::BOUNCED;
-                            break;
-
-                        case 'temporary':
-                        case 'softbounce':
-                            $type            = DoNotContact::BOUNCED;
-                            $canUseChannelId = false;
-                            break;
-
-                        default:
-                            break;
-                    }
-                    break;
-
-                case 'rejected':
-                    $type            = DoNotContact::IS_CONTACTABLE;
-                    $canUseChannelId = false;
-                    break;
-
-                case 'complained':
-                    $type = DoNotContact::UNSUBSCRIBED;
-                    break;
-
-                case 'unsubscribed':
-                    $reason = 'User unsubscribed';
-                    $type   = DoNotContact::UNSUBSCRIBED;
-                    break;
-
-                default:
-                    // Ignore any other events.
-                    break;
-            }
-
-            $channelId = null;
-            $this->logger->debug(serialize($event));
-            if (isset($event['message']['headers'])) {
-                $event['CustomID'] = $this->getEmailChannelId($event['message']['headers']);
-
-                // Make sure channel ID is always set, so data on graph is displayed correctly.
-                if (!empty($event['CustomID'])) {
-                    $channelId = (int) $event['CustomID'];
-                }
-            }
-
-            if (null !== $channelId && $canUseChannelId) {
-                $this->transportCallback->addFailureByAddress(
-                    $event['recipient'],
-                    $reason,
-                    $type,
-                    $channelId
-                );
-            } else {
-                $this->transportCallback->addFailureByAddress(
-                    $event['recipient'],
-                    $reason,
-                    $type,
-                    null
-                );
-            }
+            $this->logger->warning(
+                'OmniveryTransportCallbackSubscriber: Unrecognized event type.',
+                ['type' => $eventData['event']]
+            );
         }
 
-        $event->setResponse(new Response('Omnivery Integration: Callback processed'));
+        $recipient = $eventData['recipient'] ?? null;
+        $this->processCallbackByEmailAddress($recipient, $eventData);
+
+        $event->setResponse(
+            new Response(
+                json_encode([
+                    'message' => 'OK',
+                    'success' => true,
+                ]),
+                Response::HTTP_OK
+            )
+        );
         $this->logger->debug('End processCallbackRequest');
     }
 }
