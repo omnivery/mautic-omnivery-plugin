8370592525df30346272934fd155da12f047175b
Matic Zagmajster <maticzagmajster@gmail.com>
2026-06-15 15:03:05 +0200

    Version 5.0.0

--- Config/config.php
+++ Config/config.php
@@ -4,7 +4,7 @@ return [
     'name'        => 'OmniveryMailer',
     'description' => 'Integrate Mailer transport for Omnivery API (mautic 5)',
     'author'      => 'Matic Zagmajster',
-    'version'     => '2.0.0',
+    'version'     => '5.0.0',
 
     'services' => [
         'forms' => [
